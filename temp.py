3ffc2a48106a7d0ce911410906ef4404243e13e7
txtLee <txtlee@txtLeedeMacBook-Pro.local>
2026-07-02 18:29:33 +0800

    128 最长连续序列

--- temp.py
+++ temp.py
@@ -1,46 +1,47 @@
 """
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
 
-
-相关标签
-premium lock icon
-相关企业
-
-提示
-给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除了 nums[i] 之外其余各元素的乘积 。
-
-题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
-
-请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
 
  
 
-示例 1:
+示例 1：
 
-输入: nums = [1,2,3,4]
-输出: [24,12,8,6]
-示例 2:
+输入：nums = [100,4,200,1,3,2]
+输出：4
+解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+示例 2：
 
-输入: nums = [-1,1,0,-3,3]
-输出: [0,0,9,0,0]
+输入：nums = [0,3,7,2,5,8,4,6,0,1]
+输出：9
+示例 3：
+
+输入：nums = [1,0,1,2]
+输出：3
 """
 
 """
-好像是属于动态规划的思路了。
-要求O(N)的时间复杂度，就要求遍历1次或2次以内完成；
-每个元素都跟所有的元素相关，又要求O(N)时间复杂度，可以考虑双向遍历，即从左往右遍历一次，从右往左遍历一次；
-左右各遍历一次的过程中，可以存增量数据，即第5位存放前4位的数据、存放后n-5位的数据。
+要求O(N)时间复杂度，优先考虑接住hashmap或set
+本体思维：找每个连续序列的最小数字，只要连续序列的最小数字直接跳过，是的话则一直往上加。时间最多花费2N
+需要O(1)判断当前变量或当前变量的加减乘除变体的变量，有效考虑哈希hashmap或者集合set
 """
 
 class Solution(object):
-    def groupAnagrams(self, nums):
-        res = [] 
-        n = len(nums)
-        res[0] = 1
-        res[n - 1] = 1
-        for i in range(1, n):
-            res[i] = res[]
+    def longestConsecutive(self, nums):
+        if len(nums) == 0:
+            return 0
+        res = 0
+        num_set = set(nums)
+        for num in num_set:
+            if num - 1 not in num_set:
+                cur_num = num
+                cur_len = 1
+                while (cur_num + 1 in num_set):
+                    cur_len += 1
+                    cur_num += 1
+                if cur_len > res:
+                    res = cur_len
 
+        return res
 
-        return []
-        
         
